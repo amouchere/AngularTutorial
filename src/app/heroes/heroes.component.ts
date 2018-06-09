3900b1ac8570d5a805eb504fe394525e7b824e1b
a.mouchere <a.mouchere@gmail.com>
2018-06-09 23:23:45 +0200

    tutorial chapter5

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../heroes/hero';
+import { HEROES } from '../mock-heroes';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+ 
+  heroes = HEROES;
+
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero.name);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
